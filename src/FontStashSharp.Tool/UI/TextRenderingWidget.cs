b0f3fc4e8799366d49dbc29867f8f17234baac51
Roman Shapiro <eleroman@mail.ru>
2026-09-16 11:55:53 +0700

    Preparing fss-tool for the publish

--- src/FontStashSharp.Tool/UI/TextRenderingWidget.cs
+++ src/FontStashSharp.Tool/UI/TextRenderingWidget.cs
@@ -4,7 +4,6 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Myra;
 using Myra.Graphics2D;
-using Myra.Graphics2D.Brushes;
 using Myra.Graphics2D.UI;
 
 namespace FontStashSharp.Samples;
